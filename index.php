3a0c2154dd27fd437cd8da98303f90d1686bffcb
yadavmahima2023 <yadavmahima2023@gmail.com>
2025-01-26 17:17:42 +0530

    star program

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
-<?php echo "Welcome to GIT...."; ?>
+<?php echo "Welcome to GIT....<br>"; ?>
+<?php
+    for($i=0; $i<=5; $i++){
+        for($j=0; $j<$i; $j++){
+            echo "* ";
+        }
+        echo "<br>";
+    }
+?>
